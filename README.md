ef8e54bb21b2f00374b703afab08408467c99919
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-02-20 20:33:17 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# _just-docs
-_just Documentation.
+[![logo](https://raw.githubusercontent.com/JustDeveloper1/all.is-a.dev/refs/heads/main/public/logo-t.png)](https://all.is-a.dev/)
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+/*
+
 MIT License
 
-Copyright (c) 2025 JustDeveloper <https://justdeveloper.is-a.dev>
+Copyright (c) 2025 JustDeveloper <https://justdeveloper.is-a.dev/>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,3 +21,43 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+*/
+
+import { useEffect, useState } from 'react';
+import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/router';
+
+const Home = () => {
+  const [listItems, setListItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [failed, setFailed] = useState(false);
+  const translate = useTranslations();
+  const router = useRouter();
+
+  return (
+    <>
+      <h1 dangerouslySetInnerHTML={{
+        __html:
+          translate.raw('name')
+      }} />
+      <p dangerouslySetInnerHTML={{
+        __html:
+          translate.raw('desc')
+      }} />
+    </>
+  );
+};
+
+export default Home;
+
+export async function getStaticProps(context) {
+  const messages = (await import('/locales/' + context.locale + '.json'))
+    .default
+  return {
+    props: {
+      messages,
+      ...context,
+    },
+  }
+}
