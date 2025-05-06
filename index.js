2b2993abc0dfa44a0ba28fae4403b6d8f03d7756
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-05-06 23:30:58 +0500

    fix

--- index.js
+++ index.js
@@ -5,23 +5,3 @@ if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
     document.querySelector(".navbar").classList.remove("scroll");
     }
 };
-
-let focused;
-
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'Tab') {
-        const focusedElement = document.activeElement;
-        if (focused && focusedElement !== focused) {
-            focused.classList.remove('keyboard');
-        }
-        focused = focusedElement;
-        focusedElement.classList.add('keyboard');
-    } else {
-        focused.classList.remove('keyboard');
-    }
-});
-
-document.addEventListener('click', function() {
-    const focusedElement = document.activeElement;
-    focusedElement.classList.remove('keyboard');
-});
