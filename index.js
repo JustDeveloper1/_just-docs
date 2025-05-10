d2f90022d2d482665a4d6310876c23d4863a1518
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-05-10 16:00:09 +0500

    updates

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
     document.querySelector(".navbar").classList.remove("scroll");
     }
 };
+window.addEventListener('scroll', function() {
+    const elements = document.querySelectorAll('h1, h2, h3');
+    elements.forEach((element, index) => {
+        const rect = element.getBoundingClientRect();
+        const a = ((rect.top + rect.height / 2) <= (window.innerHeight / 2));
+        if (a) {
+            
+        } else {
+            document.body.style.setProperty('--hc', index);
+        }
+    });
+});
